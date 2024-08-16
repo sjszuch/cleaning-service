4e87fb39e0264f8262b3be603a0f6e20ea5c3a7b
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-08-16 11:48:24 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SplashComponent } from './components/splash/splash.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ServicesComponent } from './components/services/services.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { Card1Component } from './components/card1/card1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SplashComponent,
+    HeroComponent,
+    ServicesComponent,
+    ClientsComponent,
+    Card1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
